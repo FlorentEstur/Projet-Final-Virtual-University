5dec3e755bd70bf256dd24735e48dfe3eb9ab652
FlorentEstur <warcr@DESKTOP-3VUL9G4>
2022-12-26 14:24:36 +0100

    Sauvegarde XML en BDD qui fonctionne

---
+++ src/main/java/org/mycompany/process/JaxBUnmarshallBDD.java
@@ -0,0 +1,32 @@
+package org.mycompany.process;
+
+import java.io.File;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+import org.mycompany.model.SupportCours;
+import org.mycompany.repository.ISupportCoursRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JaxBUnmarshallBDD implements Processor {
+
+	@Autowired
+	ISupportCoursRepository iscr;
+
+	@Override
+	public void process(Exchange exchange) throws Exception {
+
+		JAXBContext context = JAXBContext.newInstance(SupportCours.class);
+		Unmarshaller unmarshaller = context.createUnmarshaller();
+		SupportCours s1 = (SupportCours) unmarshaller.unmarshal(new File("TestTransfoXML/Support-test.xml"));
+		iscr.save(s1);
+
+	}
+
+}
